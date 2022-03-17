2882fedaeacd370eff2880edaa9143c9002a4e6d
admin <admin@Bcalab3-m09>
2022-03-17 09:44:09 +0530

    added new method

--- Pravin210/src/STUDENT.java
+++ Pravin210/src/STUDENT.java
@@ -1,6 +1,18 @@
 
 public class STUDENT {
-
+	String name;
+	STUDENT(String name)
+	{
+		this.name =name;
+	}
+	void display()
+	{
+		System.out.println("student name" +name);
+	}
+	void print()
+	{
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
